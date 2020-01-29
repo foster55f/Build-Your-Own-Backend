0cd318932d9dd6beff5aa979404269322f2b02f7
“Foster <fosterataylor@yahoo.com>
2020-01-28 19:01:34 -0700

    Add seeding and knex data

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # NBA Teams and Players API
 ## by Foster Taylor
 [Foster Taylor](https://github.com/foster55f)
+<br>
 [BYOB Project Board](https://github.com/users/foster55f/projects/2)
---
+++ db/migrations/20200128170318_initial.js
@@ -0,0 +1,28 @@
+
+exports.up = function (knex) {
+    return Promise.all([
+        knex.schema.createTable('teams', (table) => {
+            table.increments('id').primary();
+            table.string('team');
+            table.string('conference');
+
+            table.timestamps(true, true);
+        }),
+        
+        knex.schema.createTable('players', (table) => {
+            table.increments('id').primary();
+            table.string('team');
+            table.integer('team_id').unsigned()
+            table.foreign('team_id')
+              .references('teams.id');
+      
+            table.timestamps(true, true);
+        })
+    ])  
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTable('players')
+        .dropTable('teams')
+};
