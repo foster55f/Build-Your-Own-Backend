a7da7bf5f5a4e8ecda3818b7be192589bcf5d422
“Foster <fosterataylor@yahoo.com>
2020-01-30 10:47:55 -0700

    Finish 4th GET endpoint

--- server.js
+++ server.js
@@ -39,6 +39,25 @@ app.get('/api/v1/teams/:id', async (request, response) => {
       } else {
         response.status(200).json(team[0])
       }
+    })
+    .catch(error => {
+      response.status(500).json({error})
+  })
+})
+
+app.get('/api/v1/players/:id', async (request, response) => {
+  const { id } = request.params;
+  database('players')
+    .where({ id: id })
+    .then(player => {
+      if (!player[0]) {
+        response.status(404).json({error: `no player with ${id} found`})
+      } else {
+        response.status(200).json(player[0])   
+      }
+    })
+    .catch(error => {
+    response.status(500).json({error})
   })
 })
 
